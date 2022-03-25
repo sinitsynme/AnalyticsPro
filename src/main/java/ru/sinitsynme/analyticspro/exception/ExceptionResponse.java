1dc8a77c8e16b3c1ada99a7bfee1308fdec1bac7
sinitsynme <sinitsynme@outlook.com>
2022-03-25 12:18:24 +0400

    Update ExceptionHandler

---
+++ src/main/java/ru/sinitsynme/analyticspro/exception/ExceptionResponse.java
@@ -0,0 +1,30 @@
+package ru.sinitsynme.analyticspro.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+    private Date occurrenceTime;
+    private String message;
+
+    public ExceptionResponse(Date occurrenceTime, String message) {
+
+        this.occurrenceTime = occurrenceTime;
+        this.message = message;
+    }
+
+    public Date getOccurrenceTime() {
+        return occurrenceTime;
+    }
+
+    public void setOccurrenceTime(Date occurrenceTime) {
+        this.occurrenceTime = occurrenceTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/ru/sinitsynme/analyticspro/exception/handler/AppExceptionHandlerController.java
+++ src/main/java/ru/sinitsynme/analyticspro/exception/handler/AppExceptionHandlerController.java
@@ -1,10 +1,20 @@
 package ru.sinitsynme.analyticspro.exception.handler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ru.sinitsynme.analyticspro.exception.ApplicationRegistrationException;
+import ru.sinitsynme.analyticspro.exception.ExceptionResponse;
+import ru.sinitsynme.analyticspro.exception.ResourceNotFoundException;
 import ru.sinitsynme.analyticspro.exception.UserRegistrationException;
+
+import java.util.Date;
+
 
 @ControllerAdvice
 public class AppExceptionHandlerController {
@@ -19,6 +29,25 @@ public class AppExceptionHandlerController {
     public String authenticationException(RedirectAttributes redirectAttributes){
         redirectAttributes.addAttribute("error", "Wrong email or password");
         return "redirect:/login";
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public String accessDeniedException(RedirectAttributes redirectAttributes){
+        redirectAttributes.addAttribute("error", "Oops... You don't have access to this page!");
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(ApplicationRegistrationException.class)
+    public String applicationRegistrationException(RedirectAttributes redirectAttributes, Exception ex){
+        redirectAttributes.addAttribute("error", ex.getMessage());
+        return "redirect:/applications/new";
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ExceptionResponse> resourceNotFoundException(Exception ex){
+        ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
 }
